67ef00187f923dfe667523ab74c0f21ae96af064
cloudrohu <141917927+cloudrohu@users.noreply.github.com>
2025-06-30 18:04:11 +0530

    true

--- Makaan_Hub/settings.py
+++ Makaan_Hub/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5(e@lnf^d)an2em_qk!%lf)^48y)h#8uex7k(wcx!nf#$v43tl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -138,12 +138,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/uploads/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+STATIC_ROOT = BASE_DIR / 'static'
+
+
 
 # ...
 SITE_ID = 1
